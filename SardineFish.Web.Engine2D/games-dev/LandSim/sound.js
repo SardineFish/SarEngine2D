d0e9272e763291d22ba61f2e3547426be41e4005
SardineFish <Sardine@live.cn>
2017-10-20 13:28:15 +0800

    game-dev

--- SardineFish.Web.Engine2D/games-dev/LandSim/sound.js
+++ SardineFish.Web.Engine2D/games-dev/LandSim/sound.js
@@ -1,24 +1,27 @@
 import { Entity } from "./entity.js";
 import { Animal } from "./animal.js";
 import { Global } from "./global.js";
-import { RenderableEntity } from "./renderableEntity.js"
+import { RenderableEntity } from "./renderableEntity.js";
+const SpeedOfSound = 314;
 export class Sound extends Entity
 {
-    constructor(id, sender, x, y, spreadDistance)
-    {
-        super(id, x, y);
+	constructor(id, sender, x, y, spreadDistance)
+	{
+		super(id, x, y);
 		this.sender = sender;
 		this.spreadDistance = spreadDistance;
 		var circle = new Circle(1);
-		circle.strokeStyle = new Color(128, 128,128, 0.3);
+		circle.strokeStyle = new Color(128, 128, 128, 0.3);
 		circle.fillStyle = new Color(255, 255, 255, 0.3);
 		this.gameObject.graphic = circle;
 		this.gameObject.moveTo(x, y);
-    }
+	}
+
+	static get SpeedOfSound() { return SpeedOfSound; }
 
 	onDisplay()
 	{
-	
+		this.gameObject.animate({ "graphic.r": this.spreadDistance }, this.spreadDistance / SpeedOfSound);
 	}
 
 }
